44feffd0a512077b719ea584c561ef06470ab0ca
NameRschool <hand1b20@mbjcomp.org.il>
2023-06-11 18:47:30 +0300

    fetch

--- src/comp/FromPython.js
+++ src/comp/FromPython.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 export default function FromPython() {
   const [text, setText] = useState('');
+  const [date, setDate] = useState("a");
+  // const [text, setText] = useState('');
+  // const [text, setText] = useState('');
 
 
   const fetchText = async () => {
     try {
-      const response = await fetch('http://localhost:8000/get-text'); 
+      const response = await fetch('http://127.0.0.1:5000');
       const data = await response.text();
       setText(data);
     } catch (error) {
@@ -15,7 +18,7 @@ export default function FromPython() {
   };
   const fetchNum = async () => {
     try {
-      const response = await fetch('http://localhost:8000/get-text'); 
+      const response = await fetch('http://localhost:8000/get-text');
       const data = await response.text();
       setText(data);
     } catch (error) {
@@ -25,7 +28,7 @@ export default function FromPython() {
 
   const fetchInfo = async () => {
     try {
-      const response = await fetch('http://localhost:8000/get-text'); 
+      const response = await fetch('http://localhost:8000/get-text');
       const data = await response.text();
       setText(data);
     } catch (error) {
@@ -34,16 +37,17 @@ export default function FromPython() {
   };
   const fetchDate = async () => {
     try {
-      const response = await fetch('http://localhost:8000/get-text'); 
+      const response = await fetch('http://127.0.0.1:5000/date');
       const data = await response.text();
-      setText(data);
+      debugger
+      setDate(data);
     } catch (error) {
       console.error('An error occurred:', error);
     }
   };
   const fetchFile = async () => {
     try {
-      const response = await fetch('http://localhost:8000/get-text'); 
+      const response = await fetch('http://127.0.0.1:5000/api/FileUrls');
       const data = await response.text();
       setText(data);
     } catch (error) {
@@ -53,19 +57,14 @@ export default function FromPython() {
 
   return (
     <div>
-        <text>קובץ</text>
-        <button onClick={fetchText}>Text</button>
-        <button onClick={fetchNum}>Num</button>
-        <button onClick={fetchInfo}>Info</button>
-        <button onClick={fetchDate}>Date</button>
-        <button onClick={fetchFile}>File</button>
-
-
-
-
-      <p>{text}</p>
+      <text>קובץ</text>
+      <button onClick={fetchText}>Text</button>
+      <button onClick={fetchNum}>Num</button>
+      <button onClick={fetchInfo}>Info</button>
+      <button onClick={fetchDate}>Date</button>
+      <button onClick={fetchFile}>File</button>
+      <p>{date}</p>
     </div>
   );
 }
 
-  
