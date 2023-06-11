25456de2cb55045c2feddfdfa692d7792284c2be
MyNameIsYg <hand1b10@mbjcomp.org.il>
2023-06-11 16:16:27 +0300

    get url

--- src/comp/FileUrls.js
+++ src/comp/FileUrls.js
@@ -2,56 +2,54 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function FileUrls() {
-  const [fileUrls, setFileUrls] = useState({
-    pendingUrls: [],
-    processingUrls: [],
-    completedUrls: [],
-  });
+  const [status, setStatus] = useState('');
+  const [fileUrls, setFileUrls] = useState([]);
+
+  const fetchUrlsByStatus = async (status) => {
+    try {
+      //debugger
+      const response = await axios.get(`http://localhost:5029/api/File/UrlsByStatus?status=${status}`);
+      console.log(response)
+      setFileUrls(response.data);
+    } catch (error) {
+      console.log('An error occurred:', error);
+    }
+  };
+
+  const handleStatusChange = (event) => {
+    setStatus(event.target.value);
+  };
+
+  // const handleGetUrls = () => {
+  //   fetchUrlsByStatus(status);
+  // };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const pendingResponse = await axios.get('/api/File/GetPendingUrls');
-        const processingResponse = await axios.get('/api/File/GetProcessingUrls');
-        const completedResponse = await axios.get('/api/File/GetCompletedUrls');
-
-        setFileUrls({
-          pendingUrls: pendingResponse.data,
-          processingUrls: processingResponse.data,
-          completedUrls: completedResponse.data,
-        });
-      } catch (error) {
-        console.error('An error occurred:', error);
-        // Handle the error as needed
-      }
-    };
-
-    fetchData();
-  }, []);
+    fetchUrlsByStatus(status);
+  }, [status]);
 
   return (
     <div>
-      <h2>Pending URLs:</h2>
+      <div>
+        <label htmlFor="status">Status: </label>
+        <input
+          type="text"
+          id="status"
+          value={status}
+          onChange={handleStatusChange}
+        />
+        {/* <button onClick={handleStatusChange}>GetUrls</button> */}
+      </div>
       <ul>
-        {fileUrls.pendingUrls.map((url, index) => (
-          <li key={index}>{url}</li>
-        ))}
-      </ul>
-
-      <h2>Processing URLs:</h2>
-      <ul>
-        {fileUrls.processingUrls.map((url, index) => (
-          <li key={index}>{url}</li>
-        ))}
-      </ul>
-
-      <h2>Completed URLs:</h2>
-      <ul>
-        {fileUrls.completedUrls.map((url, index) => (
+        {fileUrls.map((url, index) => (
           <li key={index}>{url}</li>
         ))}
       </ul>
     </div>
   );
 }
+
+
+
+
 
